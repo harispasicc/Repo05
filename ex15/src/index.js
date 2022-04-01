8327797a7575c2b34971ef947a638d8e730076c9
Haris Pasic <harispasic@ymail.com>
2022-04-01 16:50:40 +0200

    World Blanks

---
+++ ex15/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    //only change code below this line
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var worldBlanks = "It is nice " + "dog" + ", with a fast " + "ran " + "and " + "big " + "gizzard " + "which " + "quickly " + "absorbes" + ".";
+
+    //only change code above this line
+    return {
+      worldBlanks,
+    };
+  }
+  
+  console.log(main());
+  module.exports = main;
+  
