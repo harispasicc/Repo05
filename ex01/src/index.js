5987e1650271592c83c61b3a760a4a169e457bd8
Haris Pasic <harispasic@ymail.com>
2022-04-01 14:10:41 +0200

    Declaring string variables

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,15 @@
+function main() {
+  //only change code below this line
+  var myFirstName = "Haris, Emin";
+  var myLastName = "Pasic, Klinic";
+  var myHomeTown = "Sarajevo, Istanbul";
+  //only change code above this line
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown,
+  };
+}
 
+console.log(main());
+module.exports = main;
