76c055d81f76d5ecb7bafbae0521c1aa1f760d53
Haris Pasic <harispasic@ymail.com>
2022-04-01 16:45:56 +0200

    Use bracket notation to find the first char in string

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    //only change code below this line
+    var lastName = "Doe";
+    firstLetterOfLastName = " ";
+    var firstLetterOfLastName = lastName[0];
+    //only change code above this line
+    return {
+      firstLetterOfLastName,
+    };
+  }
+  
+  console.log(main());
+  module.exports = main;
+  
