18aec295cab6d3a68646915e58299863e2a552d9
Haris Pasic <harispasic@ymail.com>
2022-04-01 16:44:57 +0200

    Find a length of a string

---
+++ ex09/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    //only change code below this line
+    var lastName = "Doe".length;
+    var lastNameLength = lastName;
+    //only change code above this line
+    return {
+      lastNameLength,
+    };
+  }
+  
+  console.log(main());
+  module.exports = main;
+  
