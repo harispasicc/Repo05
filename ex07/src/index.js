96e97d12b7e8623e9b46fbb5c6a7aa27ad838f03
Haris Pasic <harispasic@ymail.com>
2022-04-01 16:43:24 +0200

    Constructing strings with variables

---
+++ ex07/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    //only change code below this line
+    var myName = "Haris Pasic doing ex07 day5 in src ";
+    var myString = "My name is " + myName + "and I am well!";
+    //only change code above this line
+    return {
+      myString,
+    };
+  }
+  
+  console.log(main());
+  module.exports = main;
+  
