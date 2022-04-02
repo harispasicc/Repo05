1891cfda05a7dc83b60f006089e1da2379e3348c
Haris Pasic <harispasic@ymail.com>
2022-04-02 18:48:39 +0200

    Concatenating strings with plus operator

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -2,9 +2,8 @@ function main() {
     //only change code below this line
     var myString = "This is the first string. " + "This is the second string.";
     //only change code above this line
-    return {
-      myString,
-    };
+    return myString;
+    
   }
   
   console.log(main());
